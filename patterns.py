46261258f56d0f17cdbf71da67243b82357b2a06
nithish kumar <janapalanithish1982715@gmail.com>
2026-02-05 23:29:35 +0530

    new python programms

--- patterns.py
+++ patterns.py
@@ -1,4 +1,14 @@
-for i in range(5):
-    if i == 2:
-        break
-print(i)
+# printing the upper triangle with the numbers
+for i in range(1, 6):          # rows
+    for j in range(1, i + 1):  # numbers per row
+        print(j, end="")
+    print()
+#printing the inverted teinaflw with numbers 
+for i in range(6,0,-1):
+    for j in range(1,i+1):
+        print(j , end="")
+    print()
+for i in range(6):          # rows
+    for j in range(i ):  # numbers per row
+        print(j, end="")
+    print()
