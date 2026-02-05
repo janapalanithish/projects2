d1d82cd716f37ffd8d93e28225765ac055a6edf7
nithish kumar <janapalanithish1982715@gmail.com>
2026-02-05 23:13:01 +0530

    db

--- new.sql
+++ new.sql
@@ -1,38 +1,19 @@
-CREATE TABLE myinfo(
-name TEXT,
-age INTEGER,
-gender TEXT,
-);
-INSERT INTO myinfo(name , age , gender)
-VALUES ('nithish',18,'male');
-DELETE FROM myinfo WHERE name = 'nithish';
-SELECT name FROM myinfo;
-UPDATE myinfo
-SET name = 'rutwik',
-age = 19,
-gender = 'male';
-WHERE myinfo.name = 'nithish';
-SELECT name 
-FROM myinfo
-WHERE name = 'nithish';
-#creating a new customer info
-CREATE TABLE newcustomer(
-name TEXT,
-age INTEGER,
-mail TEXT,
-gender TEXT,
-pass_word TEXT
-);
-UPDATE myinfo;
-WHERE id =1 ;
-WHERE mail = '123@gmail.com';
-SET name = 'rutwik';
-UPDATE myinfo
-INSERT INTO newcustomer(name , age , mail , gender , pass_word)
-VALUES ('nithish',18,'nithish@gmail.com','male','nithish123');
-DELETE FROM newcustomer 
-WHERE name = 'nithish';
-SELECT name FROM newcustomer;
-UPDATE newcustomer
-SET name = 'rutwik'
-WHERE name = 'nithish';
+CREATE TABLE info1(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    rate_limits INTEGER NOT NULL,
+    PLAN TEXT NOT NULL
+    );
+    INSERT INTO info1(id , name , age , email , rate_limits , PLAN)
+    VALUES (?,?,?,?,?,?);
+    SELECT name from info1;
+    UPDATE info1
+    SET rate_limits = 10
+    WHERE id = 1;
+    DELETE FROM info1
+    WHERE rate_limits > 10;
+    UPDATE info1
+    SET PLAN = 'premium' , rate_limits = 1000
+    WHERE id = 1;
