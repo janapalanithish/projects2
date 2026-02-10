8c44269766b2ff7f1ccc8f0eec29617d748bbd4a
nithish kumar <janapalanithish1982715@gmail.com>
2026-02-10 19:12:54 +0530

    basic database of nexus AI

---
+++ nexus.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE nexus;
+
+USE nexus;
+
+CREATE TABLE userinfo (
+       id INTEGER  ,
+       user_name TEXT not null ,
+       email TEXT not null,
+       password_hash TEXT 
+       );
+       INSERT INTO userinfo(id , user_name , email , password_hash )
+       VALUES (1,"nithish","123@gmail.com" , "123344");
+CREATE TABLE plans(
+	   chat_id INTEGER ,
+       model TEXT ,
+       no_searches INTEGER 
+       );
+       INSERT INTO model(chat_id , model , no_chats )
+       VALUES (1 , "free" , 10);
+       
+       UPDATE plans 
+       SET plan = "premium";
+       WHERE model = "free";
+       SET no_searches = 100 ;
+       WHERE model = "premium";
+       IF no_searches >= 100 
+        THEN DELETE FROM plans
+        WHERE no_searches >= 100;
+            /*OVER THE TASK TO BACKEND TO UPGRADE THE PLAN WITH GIVING A POPUP USING FRONTEND
+/* THE TASK IS UPTO BACKEND IF THE LIMIT REACHES 
+/*NEED TO UPDATE THE PLANS IF THE USER BUYS THE NEW VERSION 
+
+       
+       
+       
+       
+       
+       
+       
