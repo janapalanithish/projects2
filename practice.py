24a5617c763ae62d41efb67ac15d97de8302057b
nithish kumar <janapalanithish1982715@gmail.com>
2026-02-24 23:06:32 +0530

    first

--- practice.py
+++ practice.py
@@ -36,5 +36,22 @@ s1.get_thug()
 student.college()
 
 
+class members:
+     def __init__(self , count , section):
+          self.count = count 
+          self.section = section
+     def sectiona(self , n):
+          count = self.count + n
+          print("the count of section a is" , count)
+     def sectionb(self , m):
+          count = self.count + m
+          print("the count of section b is" , count)
+     def final(self):
+          return self.count 
+s2 = members(50 , "A")
+print(s2.final())
+s2.sectiona(10)
+s2.sectionb(100)
+
      
           
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,16 @@
-
-class student1:
-     @staticmethod
-     def college():
-        print("ejfhj")
+class members:
+     def __init__(self , count , section):
+          self.count = count 
+          self.section = section
+     def sectiona(self , n):
+          count = self.count + n
+          print("the count of section a is" , count)
+     def sectionb(self , m):
+          count = self.count + m
+          print("the count of section b is" , count)
+     def final(self):
+          return self.count 
+s2 = members(50 , "A")
+print(s2.final())
+s2.sectiona(10)
+s2.sectionb(100)
